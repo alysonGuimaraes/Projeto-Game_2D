a80caf4eec0cc2ead0b1214c46fca1e1bc75d995
Alyson Guimaraes <alyson.guima002@gmail.com>
2025-06-10 21:44:11 -0300

    Ajustes no cenário tutorial, ajuste no script do levelend, adição de mais uma sprite personalizada e adição de novos sprites nos tiles

--- scripts/level_end.gd
+++ scripts/level_end.gd
@@ -1,11 +1,35 @@
 extends Area2D
 
-@export var next_level = ""
+var next_level = ""
+
+var can_change_level = false
+
+enum levels {
+	TUTORIAL,
+	FIRST_LEVEL,
+	SECOND_LEVEL,
+	THIRD_LEVEL
+}
+
+@export var current_level: levels
+
 
 func _on_body_entered(body: Node2D) -> void:
-	call_deferred("load_next_scene")
+	
+	print("vamos..")
+	
+	can_change_level = true
+	
+	match current_level:
+		levels.TUTORIAL: next_level = 'scene_winter'
+		levels.FIRST_LEVEL: next_level = 'scene_tropic'
+		
+func _on_body_exited(body: Node2D) -> void:
+	can_change_level = false
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if can_change_level and event.is_action_pressed("action"):
+		load_next_scene()
 	
 func load_next_scene():
 	get_tree().change_scene_to_file("res://scenes/"+ next_level + ".tscn")
-
-		
--- scripts/tutorial_plate.gd
+++ scripts/tutorial_plate.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
-@onready var sign: Area2D = $sign
-
 @export var text_message: Array[String] = []
 
 func _unhandled_input(event: InputEvent) -> void:
-	if sign.get_overlapping_bodies().size() > 0:
-		if !DialogManager.is_message_active:
-			DialogManager.start_message(global_position, text_message)
-			
-	else:
-		if DialogManager.dialog_box != null:
-			DialogManager.dialog_box.queue_free()
-			DialogManager.is_message_active = false
+	var found = false
+	for area in get_tree().get_nodes_in_group("signs"):
+		var node = area.get_parent()
+		if area is Area2D and area.get_overlapping_bodies().size() > 0:
+			found = true
+			if !DialogManager.is_message_active:
+				DialogManager.start_message(area.global_position, node.text_message)	
+	
+	if !found and DialogManager.dialog_box != null:
+		DialogManager.dialog_box.queue_free()
+		DialogManager.is_message_active = false
