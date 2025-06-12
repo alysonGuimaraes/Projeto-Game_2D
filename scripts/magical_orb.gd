dbb27eeb74896b22225bdd66d9c1bde72d4d0e10
IsolDreamboy <edicarlosmfilho@gmail.com>
2025-06-11 22:42:36 -0300

    Revert "Isol"

--- scripts/magical_orb.gd
+++ scripts/magical_orb.gd
@@ -1,22 +1,15 @@
 extends Area2D
-@onready var magical_orb: Area2D = $"."
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-
-var speed = 150
+const SPEED = 150
 var direction = 1
 
-func _process(delta: float) -> void:
-	position.x += speed * delta * direction
+func _physics_process(delta: float) -> void:
+	position.x += SPEED * delta * direction
 
-
-func set_direction(dir):
-	direction = dir	
-	
 
 func _on_timer_timeout() -> void:
-	queue_free()
+	pass # Replace with function body.
+	
 
 func _on_area_entered(area: Area2D) -> void:
 	area.take_damage()
-	queue_free()
